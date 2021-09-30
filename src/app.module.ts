ba3871a81b1a79ef1ef79210ccd9def3124286b2
ahmed <ahmedgrati1999@gmail.com>
2021-09-30 19:57:13 +0100

    add database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfigService } from './config/DatabaseConfigService';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(databaseConfigService)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/EnvironmentVariables.ts
@@ -0,0 +1,8 @@
+export interface EnvironmentVariables {
+  PORT: number;
+  HOST: string;
+  DB_PORT: number;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
+  POSTGRES_DB: string;
+}
