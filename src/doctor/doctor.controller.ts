16c73127c139439e10127b38b4d8aabca01d0e57
ahmed <ahmedgrati1999@gmail.com>
2021-11-18 08:26:55 +0100

    complete doctor basic crud api

--- src/doctor/doctor.controller.ts
+++ src/doctor/doctor.controller.ts
@@ -11,7 +11,7 @@ import { DoctorService } from './doctor.service';
 import { CreateDoctorDto } from './dto/create-doctor.dto';
 import { UpdateDoctorDto } from './dto/update-doctor.dto';
 
-@Controller('doctor')
+@Controller('doctors')
 export class DoctorController {
   constructor(private readonly doctorService: DoctorService) {}
 
@@ -34,9 +34,13 @@ export class DoctorController {
   update(@Param('id') id: string, @Body() updateDoctorDto: UpdateDoctorDto) {
     return this.doctorService.update(+id, updateDoctorDto);
   }
-
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.doctorService.remove(+id);
+  softDelete(@Param('id') id: string) {
+    return this.doctorService.softDelete(+id);
+  }
+
+  @Get('restore/:id')
+  restore(@Param('id') id: string) {
+    return this.doctorService.restore(+id);
   }
 }
