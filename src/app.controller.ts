f09187bd964105db38d373ff42e900065515e926
JeridiOmar <50057549+JeridiOmar@users.noreply.github.com>
2021-11-29 18:03:33 +0100

    added role guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Roles } from './shared/decorators/roles.decorator';
+import { RoleEnum } from './patient/entities/role.enum';
+import { RolesGuard } from './shared/guards/roles.guard';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  @UseGuards(JwtAuthGuard)
+  @Roles(RoleEnum.PHARMACIST, RoleEnum.TECHNICIAN)
   @Get()
   getHello(): string {
     return this.appService.getHello();
