77141cb1e29a86ebad72831f273613a8348b9560
ahmed <ahmedgrati1999@gmail.com>
2022-01-14 15:05:23 +0100

    add pharmacist property types

--- src/pharmacist/dto/create-pharmacist.dto.ts
+++ src/pharmacist/dto/create-pharmacist.dto.ts
@@ -1,7 +1,9 @@
 import { CreatePatientDto } from '../../patient/dto/create-patient.dto';
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePharmacistDto extends CreatePatientDto {
   @IsNotEmpty()
+  @ApiProperty()
   pharmacyLocalisation: string;
 }
