c0a3c482e12d18ed83796ff8d9d913df8c548752
ahmed <ahmedgrati1999@gmail.com>
2022-01-14 15:05:02 +0100

    add patient property types

--- src/patient/dto/create-patient.dto.ts
+++ src/patient/dto/create-patient.dto.ts
@@ -9,50 +9,63 @@ import {
 } from 'class-validator';
 import { Gender } from '../entities/gender.entity';
 import { RoleEnum } from '../entities/role.enum';
+import { ApiProperty } from '@nestjs/swagger/dist';
 
 export class CreatePatientDto {
   // id: number;
 
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   password: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   role: RoleEnum;
 
   @IsNotEmpty()
+  @ApiProperty()
   firstName: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   lastName: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   @IsDateString()
   birthday: Date;
 
   @IsNumberString()
+  @ApiProperty()
   @IsNotEmpty()
   @Length(8, 8)
   cin: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   gender: Gender;
 
   @IsOptional()
   @IsNumberString()
+  @ApiProperty()
   @Length(8, 8)
   phoneNumber: string;
 
   @IsNotEmpty()
   @IsNumberString()
+  @ApiProperty()
   cnamId: number;
 
   @IsNotEmpty()
+  @ApiProperty()
   civilStatus: CivilStatusEnum;
 
   @IsNotEmpty()
+  @ApiProperty()
   socialStatus: string;
 }
