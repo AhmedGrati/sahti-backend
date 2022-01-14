4e4f209b58440532b3fee058ae9c3208aae2bdae
ahmed <ahmedgrati1999@gmail.com>
2022-01-14 15:02:54 +0100

    add doctor property types

--- src/doctor/dto/create-doctor.dto.ts
+++ src/doctor/dto/create-doctor.dto.ts
@@ -1,22 +1,28 @@
 import { CreatePatientDto } from '../../patient/dto/create-patient.dto';
 import { Column } from 'typeorm';
 import { IsNotEmpty, IsNumberString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateDoctorDto extends CreatePatientDto {
   @IsNotEmpty()
   @IsNumberString()
+  @ApiProperty()
   doctorNumber: number;
 
   @Column()
   @IsNotEmpty()
+  @ApiProperty()
   grade: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   speciality: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   serviceHospital: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   officeLocalisation: string;
 }
