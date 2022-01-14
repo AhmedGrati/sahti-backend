8d1d8e7f7ecfc7bbc942ac1da239db57316ddaef
ahmed <ahmedgrati1999@gmail.com>
2022-01-14 15:05:44 +0100

    add pharmacy property types

--- src/pharmacy/dto/create-pharmacy.dto.ts
+++ src/pharmacy/dto/create-pharmacy.dto.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreatePharmacyDto {
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   localization: string;
+  @ApiProperty()
   phone: string;
 }
