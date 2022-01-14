e0d2d817ca02d979a54bf07522f3ca94316a8fda
ahmed <ahmedgrati1999@gmail.com>
2022-01-14 15:03:53 +0100

    add medical record property types

--- src/medical-check-up/dto/create-medical-check-up.dto.ts
+++ src/medical-check-up/dto/create-medical-check-up.dto.ts
@@ -1,8 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateTranscriptionDto } from 'src/transcription/dto/create-transcription.dto';
 
 export class CreateMedicalCheckUpDto extends CreateTranscriptionDto {
+  @ApiProperty()
   additionalInformation: string;
+  @ApiProperty()
   doctorId: number;
+  @ApiProperty()
   medicalRecordId: number;
+  @ApiProperty()
   namesOfChronicDiseases: string[];
 }
