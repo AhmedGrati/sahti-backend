f536312f998592e06e6d15828d18c2a03b6de46f
ahmed <ahmedgrati1999@gmail.com>
2022-01-29 15:45:31 +0100

    fix trim pipe

--- src/shared/pipes/trim-data.pipe.ts
+++ src/shared/pipes/trim-data.pipe.ts
@@ -2,10 +2,12 @@ import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
 
 export class TrimDataPipe implements PipeTransform<any, any> {
   transform(value: any, metadata: ArgumentMetadata): any {
-    const { email } = value;
-    if (email) {
-      value.email = value.email?.trim().toLowerCase();
+    if (value) {
+      const { email } = value;
+      if (email) {
+        value.email = value.email?.trim().toLowerCase();
+      }
+      return value;
     }
-    return value;
   }
 }
