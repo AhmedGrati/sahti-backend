a28a375caeb82f32212441af96511ee4748fd7ca
ahmed <ahmedgrati1999@gmail.com>
2022-02-05 11:57:37 +0100

    fix non checked transcriptions

--- src/transcription/transcription.service.ts
+++ src/transcription/transcription.service.ts
@@ -72,7 +72,8 @@ export class TranscriptionService {
     return this.transcriptionRepository
       .createQueryBuilder('transcription')
       .leftJoinAndSelect('transcription.medicalCheckUp', 'medicalCheckUp')
-      .innerJoinAndSelect('medicalCheckUp.patient', 'patient')
+      .innerJoinAndSelect('medicalCheckUp.medicalRecord', 'medicalRecord')
+      .innerJoinAndSelect('medicalRecord.patient', 'patient')
       .where('patient.id = :patientId', { patientId })
       .andWhere('transcription.status = :status', {
         status: TranscriptionStatus.NOT_CHECKED,
